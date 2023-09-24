71fbe7c9c10d5d0d1cb851efb6d91e9eac5ec923
Alexandru Tatarciuc <tatarciucalexandru@gmail.com>
2023-09-24 15:05:23 +0200

    initial progress

---
+++ BoyumFoosballStats-2.0/Pages/MatchHistory/MatchHistory.razor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BoyumFoosballStats_2._0.Services.Interface;
+using BoyumFoosballStats_2._0.Shared.DbModels;
+using Microsoft.AspNetCore.Components;
+
+namespace BoyumFoosballStats_2.Pages.MatchHistory;
+
+public partial class MatchHistory
+{
+    public List<Match> Matches { get; set; }
+
+    [Inject] public IMatchCrudService MatchCrudService { get; set; } = null!;
+
+    protected override async Task OnInitializedAsync()
+    {
+        Matches = (await MatchCrudService.GetAllAsync()).Reverse().ToList();
+        await base.OnInitializedAsync();
+    }
+}
