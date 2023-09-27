4e6fc88953f821be92dcc7e6dec9ae82c92d2c06
Ronni Gaba <rga@boyum+it.com>
2023-09-27 12:48:36 +0200

    Fixed Json deserialization issue

--- BoyumFoosballStats-2.0.Shared/Models/TrueSkillRating.cs
+++ BoyumFoosballStats-2.0.Shared/Models/TrueSkillRating.cs
@@ -1,10 +1,22 @@
 ﻿using Moserware.Skills;
+using Newtonsoft.Json;
 
 namespace BoyumFoosballStats_2._0.Shared.Models;
 
 public class TrueSkillRating : Rating
 {
-    public TrueSkillRating(): base(GameInfo.DefaultGameInfo.InitialMean, GameInfo.DefaultGameInfo.InitialStandardDeviation)
+    public TrueSkillRating() : base(GameInfo.DefaultGameInfo.InitialMean,
+        GameInfo.DefaultGameInfo.InitialStandardDeviation)
+    {
+    }
+
+    public TrueSkillRating(Rating rating) : base(rating.Mean, rating.StandardDeviation, rating.ConservativeRating)
+    {
+    }
+
+    [JsonConstructor]
+    public TrueSkillRating(double mean, double standardDeviation, double conservativeRating) : base(mean,
+        standardDeviation, conservativeRating)
     {
     }
 }
