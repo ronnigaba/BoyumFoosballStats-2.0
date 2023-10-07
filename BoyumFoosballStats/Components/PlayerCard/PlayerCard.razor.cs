1de64cfda83f3f0ba3d350f8880980c0a092b0d1
Alexandru Tatarciuc <tatarciucalexandru@gmail.com>
2023-10-07 15:37:38 +0200

    Added PlayerCard

---
+++ BoyumFoosballStats/Components/PlayerCard/PlayerCard.razor.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using BoyumFoosballStats.Shared.DbModels;
+using Microsoft.AspNetCore.Components;
+
+namespace BoyumFoosballStats.Components.PlayerCard;
+
+public partial class PlayerCard
+{
+    [Parameter] public Player? SelectedPlayer { get; set; }
+    [Parameter] public EventCallback<Player?> SelectedPlayerChanged { get; set; }
+    [Parameter]
+    public RenderFragment? ChildContent { get; set; }
+
+    private async Task HandleClosePlayerCard()
+    {
+        SelectedPlayer = null;
+        await SelectedPlayerChanged.InvokeAsync(SelectedPlayer);
+    }
+}
